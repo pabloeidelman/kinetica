e06436151283ce6b8e98117ff28d75cd2790688e
pabloeidelman <pabloe@kinetica-solutions.com>
2017-04-07 10:31:56 -0300

    Cambios tratando que sea responsive

--- Kinetica/Scripts/Carousel.js
+++ Kinetica/Scripts/Carousel.js
@@ -2,4 +2,12 @@
     $('#myCarousel').carousel({
         interval: false  
     });
+
+    $('a.right carousel-control').click(function () {
+        $('.carousel').carousel('next')
+    });
+
+    $('a.left carousel-control').click(function () {
+        $('.carousel').carousel('prev')
+    });
 });
